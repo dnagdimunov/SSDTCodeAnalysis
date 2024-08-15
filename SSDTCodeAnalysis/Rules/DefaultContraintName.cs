133f5c0fd6ac157e81dc38c353d983bd609df45b
dnagdimunov <dnagdimunov@gmail.com>
2024-08-14 18:02:25 -0600

    cleanup & formatting

--- SSDTCodeAnalysis/Rules/DefaultContraintName.cs
+++ SSDTCodeAnalysis/Rules/DefaultContraintName.cs
@@ -1,51 +1,58 @@
 ﻿using Microsoft.SqlServer.Dac.CodeAnalysis;
 using Microsoft.SqlServer.Dac.Model;
+using System.Collections.Generic;
 using System.Globalization;
 using static SSDTCodeAnalysis.SSDTCodeAnalysisConstants;
 
 namespace SSDTCodeAnalysis.Rules
 {
-    [ExportCodeAnalysisRule(RuleId,
-    "Invalid or missing default constraint name",   
-    Category = "Naming",
-    RuleScope = SqlRuleScope.Element
-    ,Description = "Invalid or missing default constraint name {0} on column {1}")]
+    [ExportCodeAnalysisRule(
+        RuleId,
+        "Invalid or missing default constraint name",
+        Category = "Naming",
+        RuleScope = SqlRuleScope.Element,
+        Description = "Invalid or missing default constraint name {0} on column {1}"
+    )]
     public sealed class InvalidDefaultConstraintName : SqlCodeAnalysisRule
     {
-
         public const string RuleId = "SSDTCodeAnalysis.SR1004";
 
         public InvalidDefaultConstraintName()
         {
-            SupportedElementTypes =
-            [
+            SupportedElementTypes = new[]
+            {
                 ModelSchema.DefaultConstraint
-            ];
+            };
         }
 
-        public override IList<SqlRuleProblem> Analyze(
-            SqlRuleExecutionContext ruleExecutionContext)
+        public override IList<SqlRuleProblem> Analyze(SqlRuleExecutionContext ruleExecutionContext)
         {
-            IList<SqlRuleProblem> problems = [];
+            IList<SqlRuleProblem> problems = new List<SqlRuleProblem>();
 
             TSqlObject modelElement = ruleExecutionContext.ModelElement;
             RuleDescriptor ruleDescriptor = ruleExecutionContext.RuleDescriptor;
 
-            string elementName = Helper.GetElementName(ruleExecutionContext, modelElement);  
+            string elementName = Helper.GetElementName(ruleExecutionContext, modelElement);
+            var tableName = modelElement.GetReferenced()
+                                        .First(t => t.ObjectType.Name == "Table")
+                                        .Name
+                                        .Parts[1];
 
-            var tableName = modelElement.GetReferenced().First(t => t.ObjectType.Name == "Table").Name.Parts[1];
-            var column = modelElement.GetReferenced().First(t => t.ObjectType.Name == "Column");
-            
-            if (modelElement.Name.Parts.Count == 0  || !modelElement.Name.Parts[1].StartsWith(String.Format($"DF_{0}",tableName)) )
+            var column = modelElement.GetReferenced()
+                                     .First(t => t.ObjectType.Name == "Column");
+
+            if (modelElement.Name.Parts.Count == 0 ||
+                !modelElement.Name.Parts[1].StartsWith(string.Format("DF_{0}", tableName)))
             {
-                SqlRuleProblem problem = new(
-                    String.Format(CultureInfo.CurrentCulture,ruleDescriptor.DisplayDescription, modelElement.Name, string.Join(".", column.Name.Parts)),
-                    modelElement);
+                string columnName = string.Join(".", column.Name.Parts);
+                SqlRuleProblem problem = new SqlRuleProblem(
+                    string.Format(CultureInfo.CurrentCulture, ruleDescriptor.DisplayDescription, modelElement.Name, columnName),
+                    modelElement
+                );
                 problems.Add(problem);
             }
-            
+
             return problems;
         }
-
     }
 }
