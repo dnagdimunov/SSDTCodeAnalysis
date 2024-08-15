c81e6e49427c87f5a341aef713d8098046fe5606
dnagdimunov <dnagdimunov@gmail.com>
2024-08-14 18:00:24 -0600

    cleanup & formatting

--- SSDTCodeAnalysis/Rules/dboObjectSchema.cs
+++ SSDTCodeAnalysis/Rules/dboObjectSchema.cs
@@ -1,51 +1,54 @@
 ﻿using Microsoft.SqlServer.Dac.CodeAnalysis;
 using Microsoft.SqlServer.Dac.Model;
+using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using static SSDTCodeAnalysis.SSDTCodeAnalysisConstants;
 
 namespace SSDTCodeAnalysis.Rules
 {
-    [ExportCodeAnalysisRule(RuleId,
-    "Invalid object schema dbo",
-    Category = "Naming",
-    RuleScope = SqlRuleScope.Element
-    ,Description = "Invalid object schema dbo on object {0} ")]
+    [ExportCodeAnalysisRule(
+        RuleId,
+        "Invalid object schema dbo",
+        Category = "Naming",
+        RuleScope = SqlRuleScope.Element,
+        Description = "Invalid object schema dbo on object {0}"
+    )]
     public sealed class InvalidObjectSchema : SqlCodeAnalysisRule
     {
-
         public const string RuleId = "SSDTCodeAnalysis.SR1005";
 
         public InvalidObjectSchema()
         {
-            SupportedElementTypes =
-            [
-                ModelSchema.Table, ModelSchema.ScalarFunction, ModelSchema.View, ModelSchema.Procedure, ModelSchema.PartitionFunction
-            ];
+            SupportedElementTypes = new[]
+            {
+                ModelSchema.Table,
+                ModelSchema.ScalarFunction,
+                ModelSchema.View,
+                ModelSchema.Procedure,
+                ModelSchema.PartitionFunction
+            };
         }
 
-        public override IList<SqlRuleProblem> Analyze(
-            SqlRuleExecutionContext ruleExecutionContext)
+        public override IList<SqlRuleProblem> Analyze(SqlRuleExecutionContext ruleExecutionContext)
         {
-            IList<SqlRuleProblem> problems = [];
+            IList<SqlRuleProblem> problems = new List<SqlRuleProblem>();
 
             TSqlObject modelElement = ruleExecutionContext.ModelElement;
             RuleDescriptor ruleDescriptor = ruleExecutionContext.RuleDescriptor;
 
-            string elementName = Helper.GetElementName(ruleExecutionContext, modelElement); 
-            var objectSchema = modelElement.GetParent(DacQueryScopes.SameDatabase);
-            
-            if ((objectSchema?.Name?.Parts?[0] ?? "") == "dbo")
+            string elementName = Helper.GetElementName(ruleExecutionContext, modelElement);
+            TSqlObject objectSchema = modelElement.GetParent(DacQueryScopes.SameDatabase);
+
+            if ((objectSchema?.Name?.Parts?[0] ?? string.Empty) == "dbo")
             {
-                SqlRuleProblem problem = new(
-                    String.Format(CultureInfo.CurrentCulture,
-                        ruleDescriptor.DisplayDescription, modelElement.Name),
-                    modelElement);
+                SqlRuleProblem problem = new SqlRuleProblem(
+                    string.Format(CultureInfo.CurrentCulture, ruleDescriptor.DisplayDescription, modelElement.Name),
+                    modelElement
+                );
                 problems.Add(problem);
             }
-            
+
             return problems;
         }
-
     }
 }
