657e69c8f3ada46e6f863c28f3720b5562d30acf
dnagdimunov <dnagdimunov@gmail.com>
2024-08-24 09:39:29 -0600

    add foreign key name validation

---
+++ SSDTCodeAnalysis/Rules/ForeignKeyConstraintName.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.SqlServer.Dac.CodeAnalysis;
+using Microsoft.SqlServer.Dac.Model;
+using SSDTCodeAnalysis.Rules;
+using System.Collections.Generic;
+using System.Globalization;
+
+    [ExportCodeAnalysisRule(
+        RuleId,
+        "Invalid foreign key constraint name",
+        Category = "Naming",
+        RuleScope = SqlRuleScope.Element,
+        Description = "Invalid foreign key constraint name {0}, missing name or FK_ prefix."
+    )]
+    public sealed class ForeignKeyConstraintName : BaseConstraintNameRule
+    {
+        public const string RuleId = "SSDTCodeAnalysis.SR2003";
+
+        protected override string Prefix => "FK_";
+
+        protected override ModelTypeClass[] SupportedSchemas => new[]
+        {
+            ModelSchema.DefaultConstraint
+        };
+
+        // Constructor
+        public ForeignKeyConstraintName()
+        {
+            SupportedElementTypes = new[] { ModelSchema.ForeignKeyConstraint };
+        }
+    }
