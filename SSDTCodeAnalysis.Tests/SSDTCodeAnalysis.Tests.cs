b31473e4503c5b75e26f91ec86bed6532cce7b8c
dnagdimunov <dnagdimunov@gmail.com>
2024-08-26 11:54:38 -0600

    updated unit test class

--- SSDTCodeAnalysis.Tests/SSDTCodeAnalysis.Tests.cs
+++ SSDTCodeAnalysis.Tests/SSDTCodeAnalysis.Tests.cs
@@ -1,10 +1,14 @@
+using Microsoft.SqlServer.Dac.CodeAnalysis;
+using Microsoft.SqlServer.Dac.Model;
+
 namespace SSDTCodeAnalysis.Tests;
 
 [TestClass]
 public class SSDTCodeAnalysisTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Analyze_ShouldNotReturnProblem_WhenProcedureNameStartsWithUsp()
     {
+        
     }
 }
