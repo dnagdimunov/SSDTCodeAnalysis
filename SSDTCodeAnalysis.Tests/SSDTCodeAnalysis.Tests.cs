936ea166bb6b679c84af64b4065934245f58a8c6
dnagdimunov <dnagdimunov@gmail.com>
2024-08-29 18:29:12 -0600

    added unit tests for Proc Naming

--- SSDTCodeAnalysis.Tests/SSDTCodeAnalysis.Tests.cs
+++ SSDTCodeAnalysis.Tests/SSDTCodeAnalysis.Tests.cs
@@ -1,6 +1,8 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Dac;
 using Microsoft.SqlServer.Dac.CodeAnalysis;
 using Microsoft.SqlServer.Dac.Model;
-
 namespace SSDTCodeAnalysis.Tests;
 
 [TestClass]
@@ -9,6 +11,52 @@ public class SSDTCodeAnalysisTests
     [TestMethod]
     public void Analyze_ShouldNotReturnProblem_WhenProcedureNameStartsWithUsp()
     {
-        
+        using (var model = new TSqlModel(SqlServerVersion.Sql130, new TSqlModelOptions()))
+        {
+            model.AddObjects("CREATE PROCEDURE uspTest AS Select [col]=1;");
+
+            CodeAnalysisServiceFactory factory = new CodeAnalysisServiceFactory();
+            var ruleSettings = new CodeAnalysisRuleSettings()
+            {
+                new RuleConfiguration("SSDTCodeAnalysis.SR1004")
+            };
+
+            ruleSettings.DisableRulesNotInSettings = true;
+
+            CodeAnalysisService service = factory.CreateAnalysisService(model.Version, new CodeAnalysisServiceSettings()
+            {
+                RuleSettings = ruleSettings
+            });
+
+            CodeAnalysisResult analysisResults = service.Analyze(model);
+
+            Assert.IsTrue(analysisResults.Problems.Count == 0, "Expected no problems to be reported, but some were found.");
+        }
+    }
+
+[TestMethod]
+    public void Analyze_ShouldReturnProblem_WhenProcedureNameDoesNotStartWithUsp()
+    {
+        using (var model = new TSqlModel(SqlServerVersion.Sql130, new TSqlModelOptions()))
+        {
+            model.AddObjects("CREATE PROCEDURE procTest AS Select [col]=1;");
+
+            CodeAnalysisServiceFactory factory = new CodeAnalysisServiceFactory();
+                        var ruleSettings = new CodeAnalysisRuleSettings()
+                    {
+                        new RuleConfiguration("SSDTCodeAnalysis.SR1004")
+                    };
+            ruleSettings.DisableRulesNotInSettings = true;
+
+            CodeAnalysisService service = factory.CreateAnalysisService(model.Version, new CodeAnalysisServiceSettings()
+            {
+                RuleSettings = ruleSettings
+            });
+
+            CodeAnalysisResult analysisResults = service.Analyze(model);
+
+            Assert.IsTrue(analysisResults.Problems.Any(), "Expected problems to be reported, but none were found.");
+           
+        }
     }
 }
