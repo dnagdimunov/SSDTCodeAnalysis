a8d1cc3dde049b27e2b0e1bc0ed6e3f21b791726
dnagdimunov <dnagdimunov@gmail.com>
2024-09-03 13:32:35 -0600

    unit tests for view names

--- SSDTCodeAnalysis.Tests/SSDTCodeAnalysis.Tests.cs
+++ SSDTCodeAnalysis.Tests/SSDTCodeAnalysis.Tests.cs
@@ -59,4 +59,56 @@ public class SSDTCodeAnalysisTests
            
         }
     }
+
+[TestMethod]
+    public void Analyze_ShouldReturnProblem_WhenViewNameDoesNotStartWithvw()
+    {
+        using (var model = new TSqlModel(SqlServerVersion.Sql130, new TSqlModelOptions()))
+        {
+            model.AddObjects("CREATE VIEW someView AS Select columnname =1;");
+
+            CodeAnalysisServiceFactory factory = new CodeAnalysisServiceFactory();
+                        var ruleSettings = new CodeAnalysisRuleSettings()
+                    {
+                        new RuleConfiguration("SSDTCodeAnalysis.SR1006")
+                    };
+            ruleSettings.DisableRulesNotInSettings = true;
+
+            CodeAnalysisService service = factory.CreateAnalysisService(model.Version, new CodeAnalysisServiceSettings()
+            {
+                RuleSettings = ruleSettings
+            });
+
+            CodeAnalysisResult analysisResults = service.Analyze(model);
+
+            Assert.IsTrue(analysisResults.Problems.Any(), "Expected problems to be reported, but none were found.");
+           
+        }
+    }
+    [TestMethod]
+    public void Analyze_ShouldNotReturnProblem_WhenProcedureNameStartsWithvw()
+    {
+        using (var model = new TSqlModel(SqlServerVersion.Sql130, new TSqlModelOptions()))
+        {
+            model.AddObjects("CREATE VIEW vwSomeView AS Select [col]=1;");
+
+            CodeAnalysisServiceFactory factory = new CodeAnalysisServiceFactory();
+            var ruleSettings = new CodeAnalysisRuleSettings()
+            {
+                new RuleConfiguration("SSDTCodeAnalysis.SR1006")
+            };
+
+            ruleSettings.DisableRulesNotInSettings = true;
+
+            CodeAnalysisService service = factory.CreateAnalysisService(model.Version, new CodeAnalysisServiceSettings()
+            {
+                RuleSettings = ruleSettings
+            });
+
+            CodeAnalysisResult analysisResults = service.Analyze(model);
+
+            Assert.IsTrue(analysisResults.Problems.Count == 0, "Expected no problems to be reported, but some were found.");
+        }
+    }
+
 }
